ce1c8d90c2409e68ef320cbafb2420c2c739cbc5
andriputraedi <114983097+andriputraedi@users.noreply.github.com>
2022-10-06 08:03:14 -0700

    Add files via upload

---
+++ luas panjang.py
@@ -0,0 +1,12 @@
+print("Luas Persegi Panjang")
+
+panjang = int(input("Masukan Panjang :"))
+lebar = int(input("Masukan Lebar     :"))
+
+
+luas=panjang*lebar
+print("luas persegi panjang adalah     :",luas)
+
+keliling=2*(panjang+lebar)
+print("Keliling persegi panjang adalah :",keliling)
+
